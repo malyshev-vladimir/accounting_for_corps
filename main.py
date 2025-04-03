53e8e48b747e89467099af2701c6043284f746ab
Malyshev Vladimir <malyshev.vladimir.a@gmail.com>
2025-04-03 19:07:29 +0200

    feat: add structured transaction input with standard types and validation
    - added menu for standard transaction types (custom, deposit, drinks, fine, refund, event)
    - implemented input validation for transaction amount using Decimal with rounding to 2 decimal places
    - improved transaction description generation per type (e.g. fine from protocol, drinks, etc.)
    - added reusable functions:
      + input_transaction_amount: safe and precise input of signed Decimal values
      + input_protocol_info: collects meeting number, type (AC/CC/GCC/FCC), and semester
      + input_semester_by_choice: collects all necessary information to determine the semester and outputs it in a standardized format (e.g. WiSe24/25 or SoSe25)
    - connected all components into updated add_transaction_to_member workflow

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import json
 import os
 from datetime import datetime
 from decimal import Decimal, InvalidOperation
+
+from _decimal import ROUND_HALF_UP
+
 from models.member import Member, Title
 from services.monthly_payments import add_missing_monthly_payments
 
@@ -32,8 +35,122 @@ def input_valid_date():
             date_obj = datetime(year, mouth, day)
             return date_obj.strftime("%Y-%m-%d")
 
-        except ValueError as e:
-            print(f"Error. Please try again")
+        except ValueError as error:
+            print(f"{error}. Please try again")
+
+
+def input_transaction_amount():
+    while True:
+        value = input("Enter amount (e.g. 10.50 or -5.00): ").strip()
+        value = value.replace(",", ".")  # Replace comma with dot for decimal input to allow users using a comma)
+        try:
+            amount = Decimal(value)
+            amount = amount.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+            return amount
+        except InvalidOperation:
+            print("Invalid format. Please enter a number with up to 2 decimal places.")
+
+
+def input_protocol_info():
+    print("\nChoose protocol type:")
+    print("1. AC")
+    print("2. CC")
+    print("3. GCC")
+    print("4. FCC")
+
+    while True:
+        meeting_type_choice = input("Enter an option (1-4):")
+        if meeting_type_choice == "1":
+            meeting_type = "AC"
+            break
+        elif meeting_type_choice == "2":
+            meeting_type = "CC"
+            break
+        elif meeting_type_choice == "3":
+            meeting_type = "GCC"
+            break
+        elif meeting_type_choice == "4":
+            meeting_type = "FCC"
+            break
+        else:
+            print("Invalid choice. Please choose one of the options: 1, 2, 3, 4.")
+
+    while True:
+        meeting_number_choice = input("Enter the meeting number:").strip()
+        if meeting_number_choice.isdigit():
+            meeting_number = int(meeting_number_choice)
+            if 1 <= meeting_number <= 20:
+                break
+        print("I dont think that you had more then 20 meetings =) Please enter a number between 1 and 20.")
+
+    semester = input_semester_by_choice()
+
+    return f"{meeting_number}. {meeting_type} {semester}"
+
+
+def input_semester_by_choice():
+    print("\nChoose semester type:")
+    print("1. WiSe (Wintersemester)")
+    print("2. SoSe (Sommersemester)")
+
+    while True:
+        sem_choice = input("Enter number (1 or 2): ").strip()
+        if sem_choice == "1":
+            sem_type = "WiSe"
+
+            current_year = datetime.today().year
+            print("\nChoose year:")
+            print(f"1. {current_year - 2} / {current_year - 1}")
+            print(f"2. {current_year - 1} / {current_year}")
+            print(f"3. {current_year} / {current_year + 1}")
+            print(f"4. {current_year + 1} / {current_year + 2}")
+
+            while True:
+                year_choice_index = input("Enter number (1-4): ").strip()
+                if year_choice_index == "1":
+                    year_for_ws = str(current_year - 2)[2:] + "/" + str(current_year - 1)[2:]
+                elif year_choice_index == "2":
+                    year_for_ws = str(current_year - 1)[2:] + "/" + str(current_year)[2:]
+                elif year_choice_index == "3":
+                    year_for_ws = str(current_year)[2:] + "/" + str(current_year + 1)[2:]
+                elif year_choice_index == "4":
+                    year_for_ws = str(current_year + 1)[2:] + "/" + str(current_year + 2)[2:]
+                else:
+                    print("Invalid choice. Please choose one of the options: 1, 2, 3, 4.")
+                    continue
+                break
+
+            return f"{sem_type} {year_for_ws}"
+
+        elif sem_choice == "2":
+            sem_type = "SoSe"
+
+            current_year = datetime.today().year
+            print("\nChoose year:")
+            print(f"1. {current_year - 2}")
+            print(f"2. {current_year - 1}")
+            print(f"3. {current_year}")
+            print(f"4. {current_year + 1}")
+
+            while True:
+                year_choice_index = input("Enter number (1-4): ").strip()
+                if year_choice_index == "1":
+                    year_for_ss = str(current_year - 2)[2:]
+                elif year_choice_index == "2":
+                    year_for_ss = str(current_year - 1)[2:]
+                elif year_choice_index == "3":
+                    year_for_ss = str(current_year)[2:]
+                elif year_choice_index == "4":
+                    year_for_ss = str(current_year + 1)[2:]
+                else:
+                    print("Invalid choice. Please choose one of the options: 1, 2, 3, 4.")
+                    continue
+                break
+
+            return f"{sem_type} {year_for_ss}"
+
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
 
 
 def add_transaction_to_member():
@@ -61,16 +178,60 @@ def add_transaction_to_member():
     print("Enter transaction date: ")
     date = input_valid_date()
 
-    description = input("Description: ").strip()
-    amount_input = input("Amount (use '-' for expenses): ").strip()
+    print("\n  Choose transaction type:")
+    print("0. Eine beliebige Transaktion hinzufügen ")
+    print("1. Gutschrift")
+    print("2. Getränkeabrechnung")
+    print("3. Strafen")
+    print("4. Rückzahlung aufgrund eines Antrags auf Auslagenrückerstattung")
+    print("5. Beitrag für die Veranstaltung")
 
-    try:
-        amount = Decimal(amount_input)
-        member.add_transaction(date, description, amount)
-        save_members(members)
-        print(f"Transaction added to {name}.")
-    except Exception as e:
-        print("Error: {e}")
+    while True:
+        transaction_type = input("Enter number of the transaction (0 - 5): ").strip()
+
+        if transaction_type == "0":
+            description = input("Description: ").strip()
+            break
+
+        elif transaction_type == "1":
+            print("Enter date of the \"Gutschrift\"-transaction:")
+            date_of_transaction = input_valid_date()
+            description = f"Gutschrift von {date_of_transaction}"
+            break
+
+        elif transaction_type == "2":
+            print("Enter the date of the \"Getränkeabrechnung\":")
+            action_date = input_valid_date()
+            description = f"Getränkeabrechnung von {action_date}"
+            break
+
+        elif transaction_type == "3":
+            print("Enter the protocols info: ")
+            protocol_info = input_protocol_info()
+            description = f"Strafe von {protocol_info})"
+            break
+
+        elif transaction_type == "4":
+            print("Enter the date of the \"Antrag auf Auslagenrückerstattung\" in CC-mail")
+            action_date = input_valid_date()
+            description = f"AaA von {action_date}"
+            break
+
+        elif transaction_type == "5":
+            print("Enter the date of the event:")
+            action_date = input_valid_date()
+            event = input("Enter the event: ")
+            description = event + f" von {action_date}"
+            break
+
+        else:
+            print("Invalid choice. Please choose one of the options: 1, 2, 3, 4.")
+
+    amount = input_transaction_amount()
+
+    member.add_transaction(date, description, amount)
+    save_members(members)
+    print(f"Transaction added to [ {date}, {description}, {amount} ]")
 
 
 def load_members():
